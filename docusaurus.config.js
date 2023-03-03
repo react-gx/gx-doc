611b90ab3ec60403c72d699886b34938ae159383
dilane3 <komboudilane125@gmail.com>
2023-03-03 16:10:11 +0100

    Tutorial - Intro page OK

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/react-gx/gx-doc/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
